6e85e9dd59c873fabcb513284a995159c9eba4d9
Your Name <you@example.com>
2025-05-02 20:34:19 +0700

    config library

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from "express";
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
+    res.send("Hello world update")
 })
 
 app.get("/hao", (req, res) => {
