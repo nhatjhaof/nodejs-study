ca31f72c94c059366d2e2bb037c6f2db35c30c9b
Your Name <you@example.com>
2025-05-02 17:06:48 +0700

    running app on port

--- App.js
+++ App.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.get("/hao", (req, res) => {
+    res.send("Hello Hao")
+})
+
+app.listen(port, () => {
+    console.log(`My app is running on port : ${port}`)
+})
