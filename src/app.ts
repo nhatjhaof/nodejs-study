b72f623613f442630fdff2f513d0b04cbfd99810
Your Name <you@example.com>
2025-05-03 15:22:09 +0700

    nodejs mvc

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+// require('dotenv').config()
+import 'dotenv/config'
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello world update")
+    res.send("Hello world nodemon")
 })
 
 app.get("/hao", (req, res) => {
@@ -12,4 +14,5 @@ app.get("/hao", (req, res) => {
 
 app.listen(port, () => {
     console.log(`My app is running on port : ${port}`)
+    console.log(`My port : ${process.env.port}`)
 })
